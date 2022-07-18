e5e64d31bd20317551eaf87831036c6edfcbdef8
Usr_Prime <>
2022-07-18 20:09:35 -0300

    Criação dos arquivos básicos

---
+++ StarkMovies/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StarkMovies
+//
+//  Created by Usr_Prime on 18/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
